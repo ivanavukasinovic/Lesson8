01228301eb066dd16900cf5f0f1538eb98c1461e
Your Name <you@example.com>
2019-06-09 23:21:59 +0200

    MovieProducer exercise - added new class Plane

---
+++ src/exercises/Plane.java
@@ -0,0 +1,31 @@
+package exercises;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Plane {
+
+	public void addSnakes(List<Snake> snakes) {
+		// ?
+	}
+
+	public int getNumberOfVenomousSnakes() {
+
+		List<Snake> snakes = SnakeFactory.createListOfSnakesWithThatAreRandomlyVenomous();
+		List<Snake> venomousSnake = new ArrayList<Snake>();
+		List<Snake> unvenomousSnake = new ArrayList<Snake>();
+
+		for (Snake snake : snakes) {
+			if (snake.isVenomous()) {
+				venomousSnake.add(snake);
+			} else {
+				unvenomousSnake.add(snake);
+			}
+		}
+
+		System.out.println("Venomous snakes: " + venomousSnake.size());
+		System.out.println("Unvenomous snakes: " + unvenomousSnake.size());
+		return venomousSnake.size();
+	}
+
+}
