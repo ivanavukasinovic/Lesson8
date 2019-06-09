dbd7b92b8e04a415b0e44b84bbd0d87316e12ea5
Your Name <you@example.com>
2019-06-09 23:20:08 +0200

    DirtyWorms - code added

--- src/optional/DirtyWorms.java
+++ src/optional/DirtyWorms.java
@@ -1,13 +1,17 @@
 package optional;
+
 import java.util.ArrayList;
 
 public class DirtyWorms {
-	
+
 	public static void main(String[] args) {
 
-		class Stuff {}
-		class Worm extends Stuff {}
-		class Dirt extends Stuff {}
+		class Stuff {
+		}
+		class Worm extends Stuff {
+		}
+		class Dirt extends Stuff {
+		}
 		ArrayList<Stuff> stuffIFoundInTheYard = new ArrayList<Stuff>();
 		stuffIFoundInTheYard.add(new Worm());
 		stuffIFoundInTheYard.add(new Dirt());
@@ -18,10 +22,18 @@ public class DirtyWorms {
 
 		System.out.println(stuffIFoundInTheYard.size());
 
-		/* TODO: Clean out the dirt but keep the delicious worms. */ 
+		/* TODO: Clean out the dirt but keep the delicious worms. */
 
-		System.out.println(stuffIFoundInTheYard.size());	//should be 2
+		for (int i = stuffIFoundInTheYard.size() - 1; i >= 0; i--) {
+			if (stuffIFoundInTheYard.get(i).getClass().getSimpleName().equals("Dirt")) {
+				stuffIFoundInTheYard.remove(i);
+				System.out.println("Dirt found and removed.");
+			} else {
+				System.out.println("This is a worm.");
+			}
+		}
+
+		System.out.println(stuffIFoundInTheYard.size()); // should be 2
 
 	}
 }
-
