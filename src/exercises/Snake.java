194f02ea6068cda1ff226d2cd17b7a05aba001d4
Your Name <you@example.com>
2019-06-09 23:23:31 +0200

    MovieProducer exercise - added new class Snake

---
+++ src/exercises/Snake.java
@@ -0,0 +1,19 @@
+package exercises;
+
+public class Snake {
+
+	private boolean venomous;
+
+	public Snake(boolean venomous) {
+		this.venomous = venomous;
+	}
+
+	public boolean isVenomous() {
+		return venomous;
+	}
+
+	public void setVenomous(boolean venomous) {
+		this.venomous = venomous;
+	}
+
+}
